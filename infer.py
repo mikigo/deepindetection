6ee09b76b5041720d2ee19dd346096b3c8d867ad
mikigo <huangmingqiang@uniontech.com>
2024-11-13 15:27:00 +0800

    update

--- infer.py
+++ infer.py
@@ -5,10 +5,10 @@ current_path = opt.dirname(opt.abspath(__file__))
 print(current_path)
 # 初始化模型
 inferencer = DetInferencer(
-    model=f'{current_path}/rtmdet_tiny_8xb32-300e_coco.py',
-    weights=f'{current_path}/rtmdet_tiny_8xb32-300e_coco_20220902_112414-78e30dcc.pth',
-    device="cpu"
+    model=f'{current_path}/configs/deepin/faster-rcnn_r101_fpn_2x_coco.py',
+    weights=f'{current_path}/work_dirs/faster-rcnn_r101_fpn_2x_coco/epoch_24.pth',
+    device="cuda:0"
 )
 
 # 推理示例图片
-inferencer(f'{current_path}/demo/demo.jpg', out_dir=f"{current_path}/output")
+inferencer(f'{current_path}/data/coco/train2017/20241104095629.jpg', out_dir=f"{current_path}/outputs")
