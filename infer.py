abc4516fafe19de5456763a1a397e7299b82eb95
mikigo <huangmingqiang@uniontech.com>
2024-11-13 16:19:33 +0800

    update

--- infer.py
+++ infer.py
@@ -1,14 +1,13 @@
 from mmdet.apis import DetInferencer
-import os.path as opt
+import os.path as osp
+from pprint import pprint
 
-current_path = opt.dirname(opt.abspath(__file__))
-print(current_path)
-# 初始化模型
+current_path = osp.dirname(osp.abspath(__file__))
 inferencer = DetInferencer(
     model=f'{current_path}/configs/deepin/faster-rcnn_r101_fpn_2x_coco.py',
     weights=f'{current_path}/work_dirs/faster-rcnn_r101_fpn_2x_coco/epoch_24.pth',
     device="cuda:0"
 )
 
-# 推理示例图片
-inferencer(f'{current_path}/data/coco/train2017/20241104095629.jpg', out_dir=f"{current_path}/outputs")
+res = inferencer(f'{current_path}/data/coco/train2017/20241104095629.jpg', out_dir=f"{current_path}/outputs")
+pprint(res)
